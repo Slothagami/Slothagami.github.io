5eb8738c34429a4193b88bf5f8b1340bcac00658
Slothagami <48915129+Slothagami@users.noreply.github.com>
2020-10-06 08:33:46 +1100

    Revised First Post

---
+++ Scripts.js
@@ -0,0 +1,5 @@
+var str = document.getElementsByClassName('codeexample').innerHTML;
+var res = str.replace("i", "DIO");
+document.getElementsByClassName('codeexample').innerHTML = res;
+
+// <script type="text/javascript" src="Scripts.js"></script>
