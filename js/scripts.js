693290c6d776376c1de8fb3a298a09b47790d56e
Slothagami <48915129+Slothagami@users.noreply.github.com>
2020-10-06 19:45:49 +1100

    Posts Button and mobile again

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,26 @@
 
 jQuery(document).ready(function() { // wait untill jQuery is ready and run code in brackets.
 	// Start by hiding rescources
-	jQuery(".resources").next("ul").hide();
+	jQuery("#postHeader").next("ul").hide();
 	// Handle showing resources
-	jQuery(".resources").click(function() {
+	jQuery("#postHeader").click(function() {
 		jQuery(this).next("ul").slideToggle();
 	});
 	
 	// Add bit to code sections
-	//jQuery(".codeexample").prepend("<p> Pseudocode Example: </p>");
+	jQuery(".codeexample").prepend("<p class='codeTitle'> Pseudocode Explanation: </p>");
 	//jQuery(".codeexample").next("p").addClass("codeTitle");
 	
 	// Check for mobile
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		jQuery("p code").css({
-			"font-size" : 64
+		jQuery("p").css({
+			"font-size" : 48
+		});
+		jQuery("code").css({
+			"font-size" : 48
+		});
+		jQuery("bodydiv").css({
+			"width" : "100%"
 		});
 	}
 });
