21a7769af8f78fb337ec3dc32be4ade96a667185
Slothagami <48915129+Slothagami@users.noreply.github.com>
2020-10-06 16:43:05 +1100

    Added jQuery

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// <script type="text/javascript" src="Scripts.js"></script>
+
+// Linked files run on load
+//alert("Hello");
+
+jQuery(document).ready(function() {
+	jQuery(".comment").hide();
+});
