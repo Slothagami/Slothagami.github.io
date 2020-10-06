7ec11f9cd2c109c3986df02479af8ffe48de9de7
Slothagami <48915129+Slothagami@users.noreply.github.com>
2020-10-06 14:41:08 +1100

    Added Homepage

--- Scripts.js
+++ Scripts.js
@@ -1,5 +1,1 @@
-var str = document.getElementsByClassName('codeexample').innerHTML;
-var res = str.replace("i", "DIO");
-document.getElementsByClassName('codeexample').innerHTML = res;
-
 // <script type="text/javascript" src="Scripts.js"></script>
