f95b7fb0508482f5540a8e5506ad1c02ac3faf96
Slothagami <48915129+Slothagami@users.noreply.github.com>
2020-10-06 17:49:50 +1100

    Resources show/hide

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,47 @@
 // Linked files run on load
 //alert("Hello");
 
-jQuery(document).ready(function() {
-	jQuery(".comment").hide();
+jQuery(document).ready(function() { // wait untill jQuery is ready and run code in brackets.
+	// Start by hiding rescources
+	jQuery(".resources").next("ul").hide();
+	// Handle showing resources
+	jQuery(".resources").click(function() {
+		jQuery(this).next("ul").slideToggle();
+	});
+	
+	/*
+	// Keep put scroll in a variable
+	jQuery(window).scroll(function() { // every time you sroll
+		var windowHeight = jQuery(window).height();
+		var windowScroll = jQuery(window).scrollTop();
+		var windowBottom = windowHeight + windowScroll;
+	});
+	
+	// make a function
+	jQuery.fn.myFunction = function() {
+		return this.each(function() { // run relative to the current object
+			// can use the 'this' keyword in here.
+			
+		});
+	}
+	*/
+	
+	
+	/*
+		jQuery(this).parents(".classname").method();
+		jQuery(thing).append("<HTML>"); // adds html
+		
+		METHODS
+		theThing.methodName();
+		hide
+		fadeOut
+		slideUp
+		slideDown
+		slideToggle
+		click(function(){  }) // when clicked
+		next("typeOfThing") // goes to child
+		addClass("className")
+		append("<html>") // adds html
+		load("filename class")
+	*/
 });
