ae37d39765f33010f49bf501cd6a13b0389fde31
Slothagami <48915129+Slothagami@users.noreply.github.com>
2020-10-07 16:23:00 +1100

    More mobile adaptations

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function() { // wait untill jQuery is ready and run code 
 	// Check for mobile
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		// change font sizes
-		jQuery("h3, h2, p").css({
+		jQuery("h3, h2, p, ul, #postHeader").css({
 			"font-size" : 48
 		});
 		jQuery("code").css({
@@ -33,13 +33,22 @@ jQuery(document).ready(function() { // wait untill jQuery is ready and run code 
 			"font-size":50
 		});
 		// update navbar to mobile version
-		jQuery("#navbar a, #navbar img").css({
+		jQuery("#navbar a").css({
 			"font-size":48
 		});
 		// Update homepage
-		jQuery("#gamesButton").css({
+		jQuery("#gamesButton, #postsButton").css({
 			"float":"none",
 			"width":"100%"
 		});
+		jQuery("#wIKimg").css({
+			"width":"90%"
+		});
+		// big title bigger
+		jQuery("#bigTitle").css({
+			"font-size":"10vw"
+		});
+		// Add space at top
+		jQuery("#bodydiv").prepend("<br /><br /><br />");
 	}
 });
