f986351391b459e462522a4688baea35246c48b2
Slothagami <slothagami@gmail.com>
2021-01-10 23:41:52 +1100

    made discord helper page more informative and added animation

---
+++ smallPrograms/js/discordHelper.js
@@ -0,0 +1,31 @@
+var fps = 8,
+	bar,
+	animation = [],
+	frame = 0;
+
+window.onload=function() {
+	setInterval(animate, 1000 / fps);
+	bar = document.getElementById('example');
+	makeAnimation('c['  ,'**|**')
+	makeAnimation('cb[' ,'```|```')
+	makeAnimation('i['  ,'*|*')
+	makeAnimation('b['  ,'**|**')
+	makeAnimation('bi[' ,'***|***')
+	makeAnimation('u['  ,'__|__')
+	makeAnimation('ui[' ,'__*|*__')
+	makeAnimation('ub[' ,'__**|**__')
+	makeAnimation('ubi[','__***|***__')
+	makeAnimation('s['  ,'~~|~~')
+}
+
+function animate() {
+	bar.innerHTML = animation[frame];
+	frame++;
+	if(frame > animation.length - 1) frame = 0;
+}
+
+function makeAnimation(hotstring, result) {
+	for(var i = 0; i < hotstring.length+1; i++) animation.push(hotstring.substr(0,i) + '|')
+	for(var i = 0; i < 9; i++) animation.push(result)
+	for(var i = 0; i < 6; i++) animation.push('|')
+}
