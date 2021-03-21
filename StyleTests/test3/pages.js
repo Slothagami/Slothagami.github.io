b7b7a3c4f561cdaedeed6b3c7699ef417c11f350
Slothagami <slothagami@gmail.com>
2021-03-21 16:39:03 +1100

    initialised new look with style test 3

--- StyleTests/test3/pages.js
+++ StyleTests/test3/pages.js
@@ -1,4 +1,8 @@
 const onMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+const codeKeywords = {
+    'blue': ['true', 'false'],
+    'orange': ['if', 'for', '{', '}', 'repeat'] 
+}
 
 var banner
 
@@ -17,6 +21,34 @@ window.onload = ()=>{
         h1s[i].style.marginRight = 0
     }
 
+    // Color Code the <code> elements
+    let codes = document.getElementsByTagName('code')
+    for(let code of codes) {
+        let lines = code.innerHTML.trim().split('\n')
+        let newLines = []
+
+        for(line of lines) {
+            // Color Code Text
+            let s = line.search('//')
+            let ln = line
+
+            if(s != -1) {
+                // Insert <span class=green> at spot, and append /span
+                ln = ln.insert(s, '<span class="green">')
+                ln += '</span>'
+            }
+
+            // Color text based on codeKeywords const
+            for(let color of Object.keys(codeKeywords)) {
+                ln = colorKeywords(ln, codeKeywords[color], color)
+            }
+
+            newLines.push(ln)
+        }
+
+        code.innerHTML = newLines.join('\n')
+    }
+
     // Add Mobile Stylesheet if on mobile
     if(onMobile) {
         let link = document.createElement('link')
@@ -33,4 +65,22 @@ window.onload = ()=>{
 function resize() {
     // Resize Page Banner
     banner.style.height = window.innerHeight * 0.666 + 'px'
+}
+
+String.prototype.insert = function(index, string) {
+    if (index > 0)
+        return this.substring(0, index) + string + this.substr(index);
+
+    return string + this
+}
+
+function colorKeyword(str, keyword, color) {
+    // Colors all instances of a single word
+    return str.replaceAll(keyword, `<span class="${color}">${keyword}</span>`)
+}
+function colorKeywords(str, keywords, color) {
+    // Colors a list of words the same color
+    for(let word of keywords) 
+        str = colorKeyword(str, word, color)
+    return str
 }
