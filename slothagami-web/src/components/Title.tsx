46174e34bf304032e1949f0d677b8b10c0def25a
slothagami <slothagami@gmail.com>
2025-02-27 16:52:59 +1100

    add logo to sidebar animation

--- slothagami-web/src/components/Title.tsx
+++ slothagami-web/src/components/Title.tsx
@@ -13,7 +13,7 @@ function Title({ children, icon, size, id }: LinkProps) {
     return (
         <div className="title-box" style={{fontSize: size}} id={id}>
             {icon && <Icon icon={icon} />}
-            {children}
+            <div style={{fontSize: size}} className="title-content">{children}</div>
         </div>
     )
 }
