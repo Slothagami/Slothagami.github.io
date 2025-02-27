c4a52a37a43b24965f867ea141a309751b6d381a
slothagami <slothagami@gmail.com>
2025-02-27 18:33:10 +1100

    auto generate sidebar list

--- slothagami-web/src/App.tsx
+++ slothagami-web/src/App.tsx
@@ -8,29 +8,22 @@ type ArticleList = {
 }
 
 function App() {
-    const [article, setArticle] = useState("home")
+    const [article, setArticle] = useState("Home")
     const ARTICLES: ArticleList = {
-        "home": (
-            <>
-                <Title size="5rem"> Support Vector Machines </Title>
-                <p>Support Vector Machines (SVMs) are a type of AI. They are simpler compared to neural networks, as they only seperate data linearly.</p>
-                <p>The animation above shows the training process for an SVM learning to seperate the blue points from the orange. You can Move each point around to get a feel of how the AI reacts.</p>
-                <p>The faint gray lines either side of the dividing line represent the margin. This is how the SVM decides which line is best to seperate the data. The best line is chosen as the one that allows for the largest margin without hitting a data point.</p>
-            </>
-        ),
-        "other": (
-            <Title size="5rem"> Article #2 </Title>
-        )
+        "Home":  <Title size="5rem"> Slothagami </Title>,
+        "Support Vector Machines": <Title size="5rem"> Support Vector Machines </Title>
+    }
+
+    const make_button = (article_name: string) => {
+        return <Title key={article_name} onClick={() => setArticle(article_name)}> 
+                    <p className="link">{article_name} </p>
+               </Title>
     }
 
     return (
         <>
             <Sidebar>
-                <Title icon="/icons/git.svg"><a href="#">Github</a></Title>
-                <Title icon="/icons/itchio.svg"><a href="#">Itch.io</a></Title>
-                <Title icon="/icons/hamburger.svg">Menu</Title>
-
-                {Object.keys(ARTICLES).map((name) => <Title key={name} onClick={() => setArticle(name)}> {name} </Title>)}
+                {Object.keys(ARTICLES).map(make_button)}
             </Sidebar>
             <Content>
                 {ARTICLES[article]}
