f6a15467090703f3903608a3f67f337430f829f4
slothagami <slothagami@gmail.com>
2025-08-08 01:14:03 +1000

    add tab selector component

--- slothagami-web/src/pages/Homepage/Homepage.tsx
+++ slothagami-web/src/pages/Homepage/Homepage.tsx
@@ -1,10 +1,19 @@
 import Profile from "../../components/Profile/Profile"
+import TabSelector from "../../components/TabSelector/TabSelector"
+import type { Tab } from "../../components/TabSelector/TabSelector"
 import "./Homepage.css"
+
+const tabs: Tab[] = [
+    {id: "interactive", label: "Interactive", content: "interactive"},
+    {id: "maths", label: "Maths", content: "maths"},
+    {id: "games", label: "Games", content: "games"}
+]
 
 function Homepage() {
     return (
         <div id="content">
             <Profile />
+            <TabSelector tabs={tabs} />
         </div>
     )
 }
